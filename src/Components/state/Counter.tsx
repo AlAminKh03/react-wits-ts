75a0692cc0081884acafd2733769dab8fd823caf
unknown <alaminkhan6203@gmail.com>
2022-10-23 11:33:32 +0600

    ts added in useReducer

---
+++ src/Components/state/Counter.tsx
@@ -0,0 +1,55 @@
+import { useReducer } from "react";
+
+type CounterState={
+    count:number
+}
+type CounterChange={
+    type: 'increment' | 'decrement',
+    payload:number
+}
+type CounterReset ={
+    type:'reset',
+}
+
+type CounterAction= CounterChange | CounterReset
+
+const initialState={
+    count:0
+}
+
+const reducer=(state: CounterState,action:CounterAction)=>{
+switch (action.type) {
+    case 'increment':{
+return{
+    count:state.count + action.payload
+}
+    }     
+case 'decrement':{
+return{
+    count:state.count - action.payload
+}
+}
+    default:
+        return state;
+}
+}
+const Counter = () => {
+    const[state,dispatch]=useReducer(reducer,initialState)
+    return ( <>
+    count: {state.count}
+    <button onClick={()=>{dispatch({type:'increment',payload:10})
+    }}>
+        Increment
+    </button>
+    <button onClick={()=>{dispatch({type:'decrement',payload:10})
+    }}>
+        decrement
+    </button>
+    <button onClick={()=>{dispatch({type:'reset'})
+    }}>
+        reset
+    </button>
+    </> );
+}
+ 
+export default Counter;
